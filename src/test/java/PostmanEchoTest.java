44ec633c2fc18c9f5464d7d1d208c09979c7bb3a
Vitalii Dolmatov <dolmatov-vs@yandex.ru>
2020-07-04 15:29:24 +0300

    build fail

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -1,13 +1,13 @@
+import io.restassured.mapper.ObjectMapperType;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
-public class NewTest {
+public class PostmanEchoTest {
 
     @Test
-    void shouldReturnDemoAccounts() {
-        // Given - When - Then
-// Предусловия
+    void shouldReturnPostmanEcho() {
         given()
                 .baseUri("https://postman-echo.com")
                 .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
@@ -17,7 +17,6 @@ public class NewTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body(/* --> ваша проверка здесь <-- */)
-        ;
+                .body("data", equalTo("some datas"));
     }
 }
