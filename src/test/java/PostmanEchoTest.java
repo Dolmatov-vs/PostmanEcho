8cc54abc8a64c0ab3171106b7807ed494bfad7c1
Vitalii Dolmatov <dolmatov-vs@yandex.ru>
2020-07-04 15:30:20 +0300

    build compile

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -10,13 +10,13 @@ public class PostmanEchoTest {
     void shouldReturnPostmanEcho() {
         given()
                 .baseUri("https://postman-echo.com")
-                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
-// Выполняемые действия
+                .body("some data")
+
                 .when()
                 .post("/post")
-// Проверки
+
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some datas"));
+                .body("data", equalTo("some data"));
     }
 }
